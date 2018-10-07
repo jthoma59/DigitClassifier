205dd1fa24b4cbf14a79a51eccd474f5fb4e4d07
jthoma59 <43100018+jthoma59@users.noreply.github.com>
2018-10-07 18:27:18 -0500

    Update DigitLearning2 (2).py

--- DigitLearning2 (2).py
+++ DigitLearning2 (2).py
@@ -1,53 +1,37 @@
-#https://www.kaggle.com/c/digit-recognizer
-#https://www.youtube.com/watch?v=aZsZrkIgan0&t=502s
-
-#scikit learn is a popular ML library
-
 import numpy as np
 from sklearn.cross_validation import train_test_split
 
-#used to plot image
+#Used to plot image
 import matplotlib.pyplot as pt
 
-#to load training data set
+#To load training data set
 import pandas as pd
 
+#Import classifier
 from sklearn.tree import DecisionTreeClassifier
 
-
-
-
-#data includes label (what the digit is) and its various pixels
-#convert into 2d numpy array (READ ABOUT NUMPY AND PANDAS)
+#Data includes label (what the digit is) and its various pixels
+#convert into 2d numpy array
 data = pd.read_csv("DigitData/train.csv").as_matrix()
 
-
+#Assign classifier to clf
 clf = DecisionTreeClassifier()
 
-
-
-### TRAINING DATASET
-
+#Build dataset
 X = data[0:, 1:]
 y = data[0:, 0]
 
+#Create sets for testing and training data
 #.5 = separate into halves
 x_train, x_test, y_train, y_test = train_test_split(X,y,test_size=.5)
 
-
-#train our classifier
+#Train the classifier
 clf.fit(x_train,y_train)
 
-
-
-
-
+#Get predictions after classification
 predicitions = clf.predict(x_test)
 
-
+#See resulting accuracy
 from sklearn.metrics import accuracy_score
 print("Decision Tree: ")
 print(accuracy_score(y_test, predicitions))
-
-
-
